840aee3d37d0b9dcba31ce2b4138595d6168993b
BohdanProdanyk <126155459+BohdanProdanyk@users.noreply.github.com>
2023-10-13 12:31:49 +0300

    Update One.java

--- src/One.java
+++ src/One.java
@@ -30,10 +30,10 @@ public class One {
         int cc = xx + yy;
         return cc;
     }
-       public void process(One car1){
-           System.out.println(car1.car);
-           System.out.println(car1.speed);
-        
+    public void process(One car1){
+        System.out.println(car1.car);
+        System.out.println(car1.speed);
+
     }
 
 }
